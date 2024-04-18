220dfd7f0ca984c977cee162d2d75a76a5379ce7
Sathyanarayana Ramesh <148907900+sathya-narine@users.noreply.github.com>
2024-04-18 18:32:09 -0400

    memory chain

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 from PIL import Image
@@ -60,18 +62,24 @@ def get_conversational_chain():
 
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    provided context just say, "answer is not available in the context", don't provide the wrong answer
+    {context}
+
+    {chat_history}
+    human_input: {human_input}
 
     Answer:
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                              temperature=0.3)
+    
+    prompt = PromptTemplate(input_variables=["context","chat_history", "human_input"], template=prompt_template)
 
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    #prompt = PromptTemplate(template = prompt_template, input_variables = ["context","chat_history", "question"])
+    memory = ConversationBufferMemory(memory_key="chat_history",input_key="human_input")
+
+    chain = load_qa_chain(model, chain_type="stuff",memory=memory, prompt=prompt)
 
     return chain
 
@@ -87,8 +95,11 @@ def user_input(user_question):
 
     
     response = chain(
-        {"input_documents":docs, "question": user_question}
+        {"input_documents":docs, "human_input": user_question}
         , return_only_outputs=True)
+    
+    text_chunks = get_text_chunks(chain.memory.buffer)
+    get_vector_store(text_chunks)
 
     print(response)
     st.subheader("The Response is")
@@ -113,9 +124,9 @@ def main():
                 file_name = file.name
                 st.write(f"Uploaded File Format: {file_extension}")
 
-        if st.button("Process"):
+        # if st.button("Process"):
             if file_extension == 'pdf':
-                with st.spinner("Processing..."):
+                with st.spinner("We are getting prepared for you..."):
                     raw_text = get_pdf_text(uploaded_file)
                     text_chunks = get_text_chunks(raw_text)
                     get_vector_store(text_chunks)
